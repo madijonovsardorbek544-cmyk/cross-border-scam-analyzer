f631407b474ec80c8f33dcd80e43ffb873ab2575
madijonovsardorbek544-cmyk <madijonovsardorbek544@gmail.com>
2026-02-04 22:13:13 +0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAI2Z3yYDL_7SmeXedKlwMgr8EUl07DHJY",
+    authDomain: "cross-border-scam-analyzer.firebaseapp.com",
+    projectId: "cross-border-scam-analyzer",
+    storageBucket: "cross-border-scam-analyzer.firebasestorage.app",
+    messagingSenderId: "842057422979",
+    appId: "1:842057422979:web:cd4bd555c2873a951ce3b6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
